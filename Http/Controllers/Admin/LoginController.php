70257ae2c7dca2e3e8691e8d18f30844fd9b5abe
Minh Nhat <tieunhat19@gmail.com>
2025-04-02 19:08:05 +0700

    Revert "9h59 1/4 fix migration"
    This reverts commit c859760c

--- Http/Controllers/Admin/LoginController.php
+++ Http/Controllers/Admin/LoginController.php
@@ -53,24 +53,6 @@ class LoginController extends Controller
     {
         return Auth::guard('admin');
     }
-    protected function redirectTo()
-    {
-        // Kiểm tra xem người dùng đã đăng nhập chưa
-        if (Auth::guard('admin')->check()) {
-            // Người dùng đã đăng nhập, lấy thông tin người dùng
-            $user = Auth::guard('admin')->user();
-            // Kiểm tra nếu role_id = 1 (Admin)
-            if ($user->role_id == 1) {
-                return '/admin/dashboard'; // Chuyển hướng tới trang dashboard admin
-            }
-            // Nếu không phải admin, đăng xuất và chuyển hướng về trang chủ
-            Auth::guard('admin')->logout();
-            return redirect('/')->with('error', 'Bạn không có quyền truy cập vào trang quản trị.');
-        }
-
-        // Nếu người dùng chưa đăng nhập, chuyển hướng về trang chủ
-        return '/';
-    }
 
 
 
